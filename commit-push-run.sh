634893ded51a019f76369c289278bea6497093ac
erik_does_research <erik.mamet@gmail.com>
2026-05-04 06:05:23 -0400

    added commit-push-run.sh file for when CC is semi down

---
+++ commit-push-run.sh
@@ -0,0 +1,21 @@
+
+
+# I would like you to commit the changes in the current git repository with a commit message provided as an argument to the script, push the changes to the remote repository, and then run a specified command. If no commit message is provided, use "Auto-commit" as the default message. If no command is provided, do not run any command after pushing.
+
+#!/bin/bash
+# Usage: ./commit-push-run.bash "Your commit message"
+
+COMMIT_MESSAGE=${1:-"Auto-commit"}
+
+# Commit changes
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push
+
+# Source - https://superuser.com/a
+# Posted by Igor Feghali, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-20, License - CC BY-SA 3.0
+
+ssh -A erikmam@narval.alliancecan.ca << 'ENDSSH'
+  cd /home/erikmam/projects/def-scoulomb/erikmam/Dynamic3DGaussians && git pull && sbatch --account=def-scoulomb ./launch.slurm
+ENDSSH
