ae603c2b50661d03cc91a063223626cb9ec6e4bf
erik <erik.mamet@centralesupelec.fr>
2026-05-04 16:48:38 +0200

    getting ready to run -- changing install_DA3.sh

--- install_DA3.sh
+++ install_DA3.sh
@@ -1,22 +1,23 @@
-pip install requirements_wheels_DA3.txt --no-index
-module load python/3.10  # or your version
+# Load modules BEFORE creating/activating venv
+module load cuda/12.9 python/3.10 opencv/4.12 scipy-stack/2023b ffmpeg
 module load gcc cmake    # needed for builds
+
 
 python -m venv ~/wheel_env
 source ~/wheel_env/bin/activate
 
+pip install -r ./requirements_wheels_DA3.txt --no-index
+
 # pip install -e . 
 pip install --upgrade pip setuptools wheel build
 
-export WHEELHOUSE=$SLURM_TMPDIR/wheelhouse
+
+export WHEELHOUSE=./tmpdir/wheelhouse
 mkdir -p $WHEELHOUSE
 pip wheel evo -w $WHEELHOUSE
 
 module load cmake boost eigen
 module load colmap
-
-pip wheel --no-build-isolation git+https://github.com/nerfstudio-project/gsplat.git@0b4dddf04cb687367602c01196913cde6a743d70 -w $WHEELHOUSE
-#is this wheel being installed ?
 
 pip install --no-index --find-links=$WHEELHOUSE evo gsplat
 
